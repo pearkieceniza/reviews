dc073d1fb28083938f4f59a0a2bffaf720427498
Pearkie <pearkieveern.ceniza.21@usjr.edu.ph>
2023-10-03 14:33:43 +0800

    first commit

---
+++ src/data.ts
@@ -0,0 +1,33 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Uzumaki Naruto',
+      job: 'Hokage',
+      image: 'https://i.pinimg.com/originals/ee/61/37/ee61374e60f036d0d605c37b3a7bee8a.jpg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'Roronoa Zoro',
+      job: 'Pirate Hunter',
+      image: 'https://wallpapers-clan.com/wp-content/uploads/2022/08/zoro-pfp-1.jpg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'Tony Tony Chopper',
+      job: 'Doctor of the Straw Hat Pirates',
+      image: 'https://i.pinimg.com/originals/27/c2/eb/27c2eba8140ebe9fb49a70d46b0bf898.png',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'Kotaro Bokuto',
+      job: 'Ace of Fukurodani Volleyball',
+      image: 'https://i.quotev.com/cslphzbyaaaa.jpg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+  
+  export default reviews;
+  
